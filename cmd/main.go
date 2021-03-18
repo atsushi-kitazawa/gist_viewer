820703cf1b8cc8aca0836ac04e3468fa078e17d7
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-03-18 22:54:07 +0900

    trial implementation. local file viewer.

--- cmd/main.go
+++ cmd/main.go
@@ -2,82 +2,57 @@ package main
 
 import (
 	"fmt"
+	_ "fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	_ "strconv"
 	"strings"
-	"time"
+	_ "time"
 
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
 
-const corporate = "aaa bbb the ccc ddd eee fff ggg"
+// const corporate = "aaa bbb the ccc ddd eee fff ggg"
 
 var isTable bool = false
 
 func main() {
 	app := tview.NewApplication()
-	// create TextView
+	// create textView
 	textView := tview.NewTextView().
 		SetDynamicColors(true).SetWordWrap(true).SetChangedFunc(func() { app.Draw() })
 	textView.SetBorder(true).SetTitle("gist list")
-	// create Table
+	// create table
 	table := tview.NewTable().
 		Select(0, 0).
 		SetFixed(1, 1).
 		SetSelectable(true, false)
-	table.SetBorder(true)
+	table.SetBorder(true).SetTitle(" gist list")
+	// create box
+	description := tview.NewTextView().SetChangedFunc(func() { app.Draw() })
+	description.SetBorder(true).SetTitle(" description")
+	content := tview.NewTextView().SetChangedFunc(func() { app.Draw() })
+	content.SetBorder(true).SetTitle(" content")
+	// create flex
 	flex := tview.NewFlex().
-		AddItem(textView, 0, 1, false).
+		AddItem(table, 0, 1, true).
+		//AddItem(textView, 0 , 2, false).
 		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-			AddItem(tview.NewBox().SetBorder(true).SetTitle("description"), 0, 1, false).
-			AddItem(tview.NewBox().SetBorder(true).SetTitle("gist content"), 0, 10, false), 0, 2, false).
-		AddItem(table, 0, 3, true)
+			AddItem(description, 0, 1, false).
+			AddItem(content, 0, 10, false), 0, 2, false)
 
-	numSelections := 0
-	go func() {
-		for _, word := range strings.Split(corporate, " ") {
-			if word == "the" {
-				word = "[#ff0000]the[white]"
-			}
-			if word == "to" {
-				word = fmt.Sprintf(`["%d"]to[""]`, numSelections)
-				numSelections++
-			}
-			//fmt.Fprintf(textView, "%s ", word)
-			time.Sleep(75 * time.Millisecond)
-		}
-	}()
-
-	// set cell
-	table.SetCell(0, 0, &tview.TableCell{
-		Text:            " aaa",
-		NotSelectable:   false,
-		Align:           tview.AlignLeft,
-		Color:           tcell.ColorYellow,
-		BackgroundColor: tcell.ColorDefault,
-	})
-	table.SetCell(1, 0, &tview.TableCell{
-		Text:            " aaa",
-		NotSelectable:   false,
-		Align:           tview.AlignLeft,
-		Color:           tcell.ColorYellow,
-		BackgroundColor: tcell.ColorDefault,
-	})
-	table.SetCell(2, 0, &tview.TableCell{
-		Text:            " aaa",
-		NotSelectable:   false,
-		Align:           tview.AlignLeft,
-		Color:           tcell.ColorYellow,
-		BackgroundColor: tcell.ColorDefault,
-	})
-
-	// file list
+	// list file with table
 	files := listFiles()
-	for _, f := range files {
-		fmt.Fprintf(textView, "%s \n", " "+f.Name())
+	for i, f := range files {
+		table.SetCell(i, 0, &tview.TableCell{
+			Text:            " " + f.Name(),
+			NotSelectable:   false,
+			Align:           tview.AlignLeft,
+			Color:           tcell.ColorYellow,
+			BackgroundColor: tcell.ColorDefault,
+		})
 	}
 
 	// key bind
@@ -119,6 +94,10 @@ func main() {
 		}
 	}).SetSelectedFunc(func(row int, col int) {
 		table.GetCell(row, col).SetTextColor(tcell.ColorRed)
+		row, col = table.GetSelection()
+		cell := table.GetCell(row, col)
+		description.SetText(cell.Text)
+		content.SetText(getContent(cell.Text))
 	})
 
 	// set root
@@ -135,3 +114,27 @@ func listFiles() []os.FileInfo {
 	}
 	return files
 }
+
+func getContent(file string) string {
+	bytes, err := ioutil.ReadFile("./" + strings.Trim(file, " "))
+	if err != nil {
+		//log.Fatal(err)
+		fmt.Println(err)
+	}
+	return string(bytes)
+}
+
+//numSelections := 0
+//	go func() {
+//		for _, word := range strings.Split(corporate, " ") {
+//			if word == "the" {
+//				word = "[#ff0000]the[white]"
+//			}
+//			if word == "to" {
+//				word = fmt.Sprintf(`["%d"]to[""]`, numSelections)
+//				numSelections++
+//			}
+//			//fmt.Fprintf(textView, "%s ", word)
+//			time.Sleep(75 * time.Millisecond)
+//		}
+//	}()
